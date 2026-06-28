896b28ba2bddc53be82d3853485539d2f7fb7b03
mhirrt12 <kaletamene90@gmail.com>
2026-06-28 09:17:04 -0700

    feat:create add task API endpoint

--- backend/server.js
+++ backend/server.js
@@ -1,22 +1,74 @@
-const express = require("express");
-const mysql = require("mysql2");
-const cors = require("cors");
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const db = require("./db");
+//const db = require('./db');
 
 const app = express();
+
 app.use(cors());
 app.use(express.json());
 
-const db = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "todo_app"
+const PORT = process.env.PORT || 5500;
+
+app.get('/todos', (req, res) => {
+    db.query('SELECT * FROM todos ORDER BY id DESC', (err, result) => {
+        if (err) {
+            return res.status(500).json({
+                message: 'Database Error'
+            });
+        }
+
+        res.json(result);
+    });
 });
 
-db.connect((err) => {
-    if (err) {
-        console.log(err);
-    } else {
-        console.log("MySQL Connected");
+/*
+==================================
+ADD TASK
+==================================
+*/
+
+app.post('/todos', (req, res) => {
+
+    const { task } = req.body;
+
+    if (!task || task.trim() === '') {
+        return res.status(400).json({
+            message: 'Task cannot be empty'
+        });
     }
+
+    db.query(
+        'INSERT INTO todos(task) VALUES(?)',
+        [task],
+        (err) => {
+
+            if (err) {
+                return res.status(500).json({
+                    message: 'Failed to add task'
+                });
+            }
+
+            db.query(
+                'SELECT * FROM todos ORDER BY id DESC',
+                (err, result) => {
+
+                    if (err) {
+                        return res.status(500).json({
+                            message: 'Database Error'
+                        });
+                    }
+
+                    res.json(result);
+                }
+            );
+
+        }
+    );
+
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
 });
